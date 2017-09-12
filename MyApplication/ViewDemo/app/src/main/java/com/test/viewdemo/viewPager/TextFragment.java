93ea9cfa078ae2bbc6cf133fdb7bafddddac3db5
胥亚波 <xuyabo@kuaijiankang.com>
2017-09-12 14:16:14 +0800

    ViewPager外部数据改变，通知Fragment更新页面

--- MyApplication/ViewDemo/app/src/main/java/com/test/viewdemo/viewPager/TextFragment.java
+++ MyApplication/ViewDemo/app/src/main/java/com/test/viewdemo/viewPager/TextFragment.java
@@ -2,25 +2,34 @@ package com.test.viewdemo.viewPager;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class TextFragment extends  android.support.v4.app.Fragment{
-    public static TextFragment newInstance(String text){
-        TextFragment textFragment=new TextFragment();
-        Bundle bundle=new Bundle();
-        bundle.putString("text",text);
-        textFragment.setArguments(bundle);
-        return  textFragment;
+    private static final String TAG="TextFragment";
+    private String mText;
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+
+    public TextFragment() {
+        Log.i(TAG, "TextFragment 创建 ");
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         TextView textView=new TextView(getContext());
-        textView.setText(getArguments().getString("text"));
+        textView.setText(mText);
         return textView;
     }
 }
