820915fd5d7a34d55592313eed3f7886382fc13b
胥亚波 <xuyabo@kuaijiankang.com>
2017-09-14 13:53:08 +0800

    CanDragedView修改

--- MyApplication/ViewDemo/app/src/main/java/com/test/viewdemo/xview/CanDragedView.java
+++ MyApplication/ViewDemo/app/src/main/java/com/test/viewdemo/xview/CanDragedView.java
@@ -11,20 +11,21 @@ import android.view.ViewConfiguration;
  * Created by xuyabo on 2017/9/10.
  */
 
-public class CanDragedViw extends View {
+public class CanDragedView extends View {
     private boolean mIsDragging;
     private int mPointActivtyId = -1;
     private int mTouchSlop = -1;//用来判断手势移动的距离是否达到滑动的标准
-    private float mLastY=-1;
-    public CanDragedViw(Context context) {
+    private float mLastY = -1;
+
+    public CanDragedView(Context context) {
         super(context);
     }
 
-    public CanDragedViw(Context context, @Nullable AttributeSet attrs) {
+    public CanDragedView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public CanDragedViw(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public CanDragedView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
@@ -36,27 +37,27 @@ public class CanDragedViw extends View {
             //就认为是滑动。
             mTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
         }
-        switch (event.getAction()) {
+        //int action = MotionEventCompat.getActionMasked(event);
+        int action = event.getAction();
+        switch (action) {
             case MotionEvent.ACTION_DOWN:
-                mPointActivtyId=event.getPointerId(0);
-                mLastY=event.getY();
+                mPointActivtyId = event.getPointerId(0);
+                mLastY = event.getRawY();
                 break;
             case MotionEvent.ACTION_MOVE:
-                int pointIndex=event.findPointerIndex(mPointActivtyId);
-                if(pointIndex==-1){
+                int pointIndex = event.findPointerIndex(mPointActivtyId);
+                if (pointIndex == -1) {
                     break;
                 }
-                float y=event.getY(pointIndex);
-                float dy=Math.abs(y-mTouchSlop);
-                if(dy>mTouchSlop){
-                    setTranslationY(getY()+dy);
-                    mLastY=dy;
-                }
+                float y = event.getRawY();
+                float dy = y - mLastY;
+                setTranslationY(getTranslationY() + dy);
+                mLastY = y;
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                mPointActivtyId=-1;
-                mLastY=-1;
+                mPointActivtyId = -1;
+                mLastY = -1;
                 break;
         }
         return true;
