3bfbc42103ae58dfb758e426734d9be994b229b5
xuyabo <1585854768@qq.com>
2017-11-18 12:13:47 +0800

    初始工程

---
+++ FourComponent/app/src/main/java/com/example/xuyabo/fourcomponent/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.xuyabo.fourcomponent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
