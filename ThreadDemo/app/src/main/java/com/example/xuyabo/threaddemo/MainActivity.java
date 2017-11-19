a6d83ad557e43c0258af7d03404cf1736b79865e
xuyabo <1585854768@qq.com>
2017-11-19 22:48:38 +0800

    基本的Handler构造函数参数处理消息

--- ThreadDemo/app/src/main/java/com/example/xuyabo/threaddemo/MainActivity.java
+++ ThreadDemo/app/src/main/java/com/example/xuyabo/threaddemo/MainActivity.java
@@ -25,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private Handler mHandler2=new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            int what = msg.what;
+            if (what == 0) {
+                mAcceptTextView.setText("收到消息 " + msg.obj);//主线程收到消息，更新UI
+                Log.i(TAG, "线程 "+Thread.currentThread()+" 收到消息消息");
+                return true;
+            }
+            return false;
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,13 +78,13 @@ public class MainActivity extends AppCompatActivity {
                 final  String msg="大家好";
 
                 //子线程发送事件
-                mHandler.postDelayed(new Runnable() {
+                mHandler.post(new Runnable() {
                     @Override
                     public void run() {
                         mAcceptTextView.setText(msg);//主线程处理事件
                         Log.i(TAG, "线程 "+Thread.currentThread()+" 执行事件");
                     }
-                },1000);
+                });
                 Log.i(TAG, "线程 "+Thread.currentThread()+" 发送事件");
             }
         }).start();
