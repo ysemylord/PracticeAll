e9f1e91a627646c10ac16bd47aa3183c774984b2
xuyabo <1585854768@qq.com>
2018-02-28 15:19:54 +0800

    将Activity设置为透明

---
+++ FourComponent/app/src/main/java/com/example/xuyabo/fourcomponent/activityStyle/TranslucentActivity.java
@@ -0,0 +1,15 @@
+package com.example.xuyabo.fourcomponent.activityStyle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.xuyabo.fourcomponent.R;
+
+public class TranslucentActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translucent);
+    }
+}
---
+++ FourComponent/app/src/main/java/com/example/xuyabo/fourcomponent/activityStyle/TranslucentAppcompatActivity.java
@@ -0,0 +1,15 @@
+package com.example.xuyabo.fourcomponent.activityStyle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.xuyabo.fourcomponent.R;
+
+public class TranslucentAppcompatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translucent_appcompat);
+    }
+}
